e4502c9faff96b8b5fce93c860238ed52d662cf8
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-05-16 15:09:35 +0630

    added some features

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const apiKey = "0ab343c872b4fa41edc5d7d899b367b3";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 const defaultCity = "Yangon";
 
@@ -10,22 +12,40 @@ async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   const data = await response.json();
 
-  console.log(data);
+  //   console.log(data);
 
   if (data.cod === 200) {
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = data.main.temp + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
   } else {
     alert("City not found");
   }
 }
 
-window.addEventListener('load', () => {
-    checkWeather(defaultCity);
-})
+window.addEventListener("load", () => {
+  checkWeather(defaultCity);
+});
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
 });

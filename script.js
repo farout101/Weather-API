b2a26b5def119668315f8f626ce3f20773632d5f
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-05-16 15:34:35 +0630

    updated

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apiKey = "0ab343c872b4fa41edc5d7d899b367b3";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -9,43 +8,53 @@ const weatherIcon = document.querySelector(".weather-icon");
 const defaultCity = "Yangon";
 
 async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  const data = await response.json();
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  //   console.log(data);
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".city").innerHTML = "unknown";
+        document.querySelector(".temp").innerHTML = 0 + "°C";
+        document.querySelector(".humidity").innerHTML = 0 + "%";
+        document.querySelector(".wind").innerHTML = 0 + " km/h";
+        weatherIcon.src = "images/search.png";
+    } else {
+        var data = await response.json();
 
-  if (data.cod === 200) {
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        // console.log(data);
 
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+
+        document.querySelector(".error").style.display = "none";
+
+        // document.querySelector(".weather").style.display = "block";
     }
-  } else {
-    alert("City not found");
-  }
 }
 
 window.addEventListener("load", () => {
-  checkWeather(defaultCity);
+    checkWeather(defaultCity);
 });
 
 searchBtn.addEventListener("click", () => {
-  checkWeather(searchBox.value);
+    checkWeather(searchBox.value);
 });
 
 searchBox.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    checkWeather(searchBox.value);
-  }
+    if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
 });
